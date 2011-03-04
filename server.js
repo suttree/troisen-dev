9855081b2790ebd304ed65d6084495e49214f53f
Duncan Gough <duncan@suttree.com>
2011-03-04 20:33:00 +0000

    Seems to be a problem with websockets working over 3G connections, as node.js returns WebSocket connection invalid or Origin not verified messages. Upgrading the socket.io client and doing some shotgun debugging has it working where the invalid websocket connection downgrades to xhr-polling, which isn't ideal but at least it works for now.

--- server.js
+++ server.js
@@ -10,27 +10,32 @@ fs.closeSync(pidfile);
 
 var nlog = require(__dirname + '/lib/logging');
 
-function log(message) {    
-  nlog.updateAccessLog(message);    
-} 
+function log(message) {
+  nlog.updateAccessLog(message);
+}
 
-server = http.createServer(function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1>dev.troisen.com</h1>');
-  res.end();
+server = http.createServer(function(req, res){ 
+  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.write('<h1>dev.troisen.com</h1>'); 
+  res.end(); 
 });
 
 var socket = io.listen(server);
+//var socket = io.listen(server, {origins: '*:*'}); 
 
 socket.on('connection', function(client) {
   log("<"+client.sessionId+"> connected");
+  client.send('hi');
 
   client.on('message', function(evt) {
-    client.broadcast('pong');
+    log("<"+client.sessionId+"> "+evt);
+    client.broadcast('hi2');
   })
 
   client.on('disconnect', function() {
-  })
+    log("closed connection: " + client.sessionId);
+    client.broadcast('bye');
+  }) 
 });
 
-server.listen(19760, "173.45.236.98");
+server.listen(1977, '173.45.236.98');
