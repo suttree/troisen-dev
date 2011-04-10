8d6ebeec947f66947c25415bd8180a1fac314555
Duncan Gough <duncan@suttree.com>
2011-04-10 20:31:12 +0100

    More advanced EventMachine tcp server that works better over 3G on an iPhone. This is a chat server rather than a straight echo server, from http://groups.google.com/group/eventmachine/browse_thread/thread/d6173d30236fef5d

--- test.rb
+++ test.rb
@@ -1,7 +1,44 @@
-require 'socket'
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'eventmachine'
 
-server = TCPServer.new('dev.troisen.com', '1977')
-socket = server.accept
-line = socket.readline
-socket.puts line
-puts line
+module ChatClient
+  def self.list
+    @list ||= []
+  end
+
+  def post_init
+    @name = "anonymous_#{rand(99999)}"
+    puts "-- #{@name} connected to the chat server!"
+    ChatClient.list.each{ |c| c.send_data "#{@name} has joined.\n" }
+    ChatClient.list << self
+  end
+
+  def receive_data data
+    (@buf ||= '') << data
+    while line = @buf.slice!(/(.+)\r?\n/)
+      if line =~ %r|^/nick (.+)|
+        new_name = $1.strip
+        (ChatClient.list - [self]).each{ |c| c.send_data "#{@name} is now known as #{new_name}\n" }
+        @name = new_name
+      elsif line =~ %r|^/quit|
+        close_connection
+      else
+        # echo just to test
+        send_data "#{@name}: #{line}"
+        (ChatClient.list - [self]).each{ |c| c.send_data "#{@name}: #{line}" }
+      end
+    end
+  end
+
+  def unbind
+    puts "-- #{@name} disconnected from the echo server!"
+    ChatClient.list.delete self
+    ChatClient.list.each{ |c| c.send_data "#{@name} has left.\n" }
+  end
+end
+
+EventMachine::run {
+  EventMachine::start_server "dev.troisen.com", 1977, ChatClient
+  puts 'running chat server 1977'
+}
